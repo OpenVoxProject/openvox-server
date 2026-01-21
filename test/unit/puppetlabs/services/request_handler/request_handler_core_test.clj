fe72a983247b8d6da05cfc18feda794d8413a234
Corporate Gadfly <haroon.rafique@gmail.com>
2026-01-20 19:21:03 -0500

    Revert "Update test for new ring-codec"
    This reverts commit a481ee32561ff913ae6da67fe7197bb2fd585e76.

--- test/unit/puppetlabs/services/request_handler/request_handler_core_test.clj
+++ test/unit/puppetlabs/services/request_handler/request_handler_core_test.clj
@@ -265,8 +265,11 @@
   (testing "A cert provided in the x-client-cert header that cannot be decoded into
             an X509Certificate object throws the expected failure"
     (testing "Improperly URL encoded content"
-      (is (thrown+? [:kind :bad-request
-                     :msg "No certs found in PEM read from x-client-cert"]
+      (is (thrown+? #(and
+                       (= (:kind %) :bad-request)
+                       (re-matches
+                         #"Unable to URL decode the x-client-cert header: For input string: \"1Q\".*"
+                         (:msg %)))
                     (jruby-request-with-client-cert-header "%1Q%2"))))
     (testing "Bad certificate content"
       (is (thrown+? #(and
