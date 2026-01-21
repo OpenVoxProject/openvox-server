f0ea2bc914318d3426d6f1f68b0c4199037c6052
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-21 06:50:35 +0000

    Version 8.12.1

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
 
 ;; If you modify the version manually, run scripts/sync_ezbake_dep.rb to keep
 ;; the ezbake dependency in sync.
-(defproject org.openvoxproject/puppetserver "8.12.1-SNAPSHOT"
+(defproject org.openvoxproject/puppetserver "8.12.1"
   :description "OpenVox Server"
 
   :license {:name "Apache License, Version 2.0"
@@ -253,7 +253,7 @@
                                                [org.openvoxproject/jruby-utils]
                                                ;; Do not modify this line. It is managed by the release process
                                                ;; via the scripts/sync_ezbake_dep.rb script.
-                                               [org.openvoxproject/puppetserver "8.12.1-SNAPSHOT"]
+                                               [org.openvoxproject/puppetserver "8.12.1"]
                                                [org.openvoxproject/trapperkeeper-webserver-jetty10]
                                                [org.openvoxproject/trapperkeeper-metrics]]
                       :plugins [[org.openvoxproject/lein-ezbake ~(or (System/getenv "EZBAKE_VERSION") "2.7.2")]]
@@ -264,7 +264,7 @@
                                                     [org.openvoxproject/jruby-utils]
                                                     ;; Do not modify this line. It is managed by the release process
                                                     ;; via the scripts/sync_ezbake_dep.rb script.
-                                                    [org.openvoxproject/puppetserver "8.12.1-SNAPSHOT"]
+                                                    [org.openvoxproject/puppetserver "8.12.1"]
                                                     [org.openvoxproject/trapperkeeper-webserver-jetty10]
                                                     [org.openvoxproject/trapperkeeper-metrics]]
                             :uberjar-exclusions [#"^org/bouncycastle/.*"]
