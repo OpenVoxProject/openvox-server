2f6f28df644128d7138520f628ba1e16e28f9835
Charlie Sharpsteen <charlie@overlookinfratech.com>
2026-07-02 12:26:06 -0500

    Revert "Bump Xmx to 4G and increase length of test timeout"
    This commit reverts changes that gave tests more resources in the hopes
    that they would start completing after an upgrade to JRuby 9.4.14.0.
    The tests did not complete as that JRuby version induced a memory leak.
    
    Therefore, reduce resource limits back to their original values under
    OpenVox Server 8.11.0 so that future performance regressions have a
    better chance of showing up as failed or slow tests.
    
    This reverts commit 332b0166310d2f7d0011911da31ddf33375dbac6.
    
    Signed-off-by: Charlie Sharpsteen <charlie@overlookinfratech.com>

--- project.clj
+++ project.clj
@@ -234,7 +234,7 @@
                     ;; use core.async and need more than eight threads to run
                     ;; properly; this setting overrides the default value.  Without
                     ;; it the metrics tests will hang.
-                    :jvm-opts ["-Dclojure.core.async.pool-size=50", "-Xms4g", "-Xmx4g"]
+                    :jvm-opts ["-Dclojure.core.async.pool-size=50"]
                     ;; Use humane test output so you can actually see what the problem is
                     ;; when a test fails.
                     :dependencies [[pjstadig/humane-test-output]]
